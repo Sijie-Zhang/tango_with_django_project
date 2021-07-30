ba6d809f2337d47a4a850a0cca4230d49421c2c2
Sijie-Zhang <15061282152@163.com>
2021-07-30 17:04:21 +0800

    initial commit

--- rango/models.py
+++ rango/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     NAME_MAX_LENGTH = 128
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-   
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural =  'Categories'
@@ -14,8 +19,9 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
-    TITLE_MAX_LENGTH = 200
-    URL_MAX_LENGTH = 250
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+    
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharFieldmax_length=TITLE_MAX_LENGTH)
     url = models.URLField()
@@ -23,3 +29,11 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+        
+    class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
